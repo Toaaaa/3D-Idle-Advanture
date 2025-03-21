69267c1d6b40b59776917f27642481db445ef0c6
kim minjun <k980128@naver.com>
2025-03-21 15:26:54 +0900

    feat : Directory made with base scripts.

---
+++ Assets/0.Scripts/Manager/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static GameManager instance;
+    public static GameManager Instance;
+    public Player player;
+
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
